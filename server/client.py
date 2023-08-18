8da8fc422da5515bbe48660a3793334570de1d11
greenturtle537 <greenturtle537@mail.com>
2023-08-18 04:00:35 +0000

    revert server

--- server/client.py
+++ server/client.py
@@ -1,13 +1,11 @@
 import socket
 
-HOST = "glitchtech.top"
-# The server's hostname or IP address
-PORT = 8
-# The port used by the server
+HOST = "glitchtech.top"  # The server's hostname or IP address
+PORT = 8  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+  s.connect((HOST, PORT))
+  s.sendall(b"Hello, world")
+  data = s.recv(1024)
 
 print(f"Received {data!r}")
