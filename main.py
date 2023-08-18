579764bc674c45f1e0f6bc4a03dff4fffcd31f11
greenturtle537 <greenturtle537@mail.com>
2023-08-18 03:22:32 +0000

    Oh god the conflicts begin

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import codeAssets.addons.colorcalc as colorcalc
 
 class piecename:
   def __init__(self):
-    self.moves = [[0,1]]
+    self.movePath = ((0, 1))
+    self.attackPath = ((0, 1))
+
   def onAttack(self): pass
+
   def attack(self, tile): pass
