de71258d607781e66997ca38b175e57dd96a5906
greenturtle537 <greenturtle537@mail.com>
2023-08-18 03:32:15 +0000

    Server framework

--- main.py
+++ main.py
@@ -3,17 +3,35 @@ import codeAssets.addons.easyjson as json
 import codeAssets.addons.colorcalc as colorcalc
 
 
-class piecename:
+# global main game vars
+Pieces = []
+Board = []
+
+
+
+
+
+Pieces = []
+class piece:
+  def __init__(self, place: tuple[2]):
+    self.movePath = ((0,0))
+    self.attackPath = ((0,0))
+    self.coords = []
+
+  def remove(self): Pieces.remove(self)
+
+
+
+class pawn(piece):
   def __init__(self):
     self.movePath = ((0,1))
-    self.attackPath = ((1,1),())
+    self.attackPath = ((-1,1),(1,1))
 
-  def onAttack(self): pass
+  def onAttacked(self): pass
 
-  def attack(self, tile): pass
-
-
+  def Attack(self, tile): pass
 
 
 
-#Don't touch the merge conflicts
+
+
---
+++ server/client.py
@@ -0,0 +1,13 @@
+import socket
+
+HOST = "glitchtech.top"
+# The server's hostname or IP address
+PORT = 8
+# The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
