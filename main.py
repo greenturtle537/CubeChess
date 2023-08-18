96f9093de98a1d9b0200909da1bce76158c504cb
CharlesSommers <9272049-CharlesSommers@users.noreply.replit.com>
2023-08-18 15:54:10 +0000

    Added more folders for future data processing

--- main.py
+++ main.py
@@ -5,31 +5,36 @@ import codeAssets.addons.colorcalc as colorcalc
 
 # global main game vars
 Pieces = []
-Board = []
 
-
+class Board:
+  def __init__(self, validlocations: tuple[tuple]):
+    self.validlocations = validlocations
+    
 
 
 
 Pieces = []
 class piece:
-  def __init__(self, place: tuple[2]):
-    self.movePath = ((0,0))
-    self.attackPath = ((0,0))
-    self.coords = []
+  def __init__(self, movePaths: tuple[tuple[2]], attackPaths: tuple[tuple[2]], id: str):
+    Pieces.append(self)
+    self.movePath = ()
+    self.attackPath = ()
+    self.id = ()
+    self.coords = ()
 
   def remove(self): Pieces.remove(self)
 
 
 
-class pawn(piece):
-  def __init__(self):
-    self.movePath = ((0,1))
-    self.attackPath = ((-1,1),(1,1))
 
-  def onAttacked(self): pass
 
-  def Attack(self, tile): pass
+
+# class pawn(piece):
+#   def __init__(self):
+#     self.movePath = ((0,1))
+#     self.attackPath = ((-1,1),(1,1))
+#   def onAttacked(self): pass
+#   def Attack(self, tile): pass
 
 
 
