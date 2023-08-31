3ebaeb586eef203127789647c104f286c3483a2a
greenturtle537 <greenturtle537@mail.com>
2023-08-31 23:08:17 +0000

    Argh

--- server/client.py
+++ server/client.py
@@ -7,6 +7,10 @@ def en(input):
   return base62.encodebytes(bytes(input, "utf-8"))
 
 
+print("You have connected to GlitchChat")
+username = input("Please enter your username: ")
+
+
 def users():
   r = requests.get("http://glitchtech.top:8/users")
   req = r.json()
@@ -18,11 +22,6 @@ def keepalive(userid):
                    params={"username": userid})
   req = r.json()
   return req
-
-
-print("You have connected to GlitchChat")
-username = input("Please enter your username: ")
-
 r = requests.get("http://glitchtech.top:8/connect",
                  params={"username": username})
 req = r.json()
