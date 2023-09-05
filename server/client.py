0416bf6a37b5bbfd971b4b78344b475572ef6e92
greenturtle537 <greenturtle537@mail.com>
2023-09-05 06:20:15 +0000

    Added quite a few helper routines

--- server/client.py
+++ server/client.py
@@ -1,6 +1,11 @@
 import requests
 import base62
 from time import sleep
+import kbhit
+
+
+def cls():
+  os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def en(input):
@@ -22,11 +27,29 @@ def keepalive(userid):
                    params={"username": userid})
   req = r.json()
   return req
+
+
 r = requests.get("http://glitchtech.top:8/connect",
                  params={"username": username})
 req = r.json()
 print(req)
 #Frameloop sequence ahead, todo error handling
+buffer = []
+
+
+def write(message):
+  buffer.append(message)
+
+
+def cleaner():
+  cls()
+  for msg in buffer():
+    print(msg)
+  if len(buffer) > 50:
+    buffer.pop(0)
+
+
 while req["result"]:
-  print(keepalive(username))
+  write(keepalive(username))
   sleep(0.5)
+  cleaner()
