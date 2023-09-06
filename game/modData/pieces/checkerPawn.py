4d13b8464c6a110d0113ac6486efd6c866b608d0
greenturtle537 <greenturtle537@mail.com>
2023-09-06 19:13:06 +0000

    File changes

---
+++ game/modData/pieces/checkerPawn.py
@@ -0,0 +1,18 @@
+attackPath = []
+movePath = [(1,-1),(-1,-1)]
+canPromote = True
+canJumpPieces = True
+canJumpEmpties = True
+char = 'c'
+promotesTo = ['C']
+
+conditionalMovePath = [
+  ((-2,-2), lambda p, b, g: b.PieceAt(p.pos[0]-1, p.pos[1]-1)[1])
+  ((-2,-2), lambda p, b, g: b.PieceAt(p.pos[0]+1, p.pos[1]+1)[1])
+]
+conditionalAttackPath = []
+def onMove(self, game, board, pieces, oldPos, newPos): 
+  if self.pastMoves[-1][2] == (-2,-2*(-1 if self.color-1 else 1)): p = board.PieceAt(newPos[0]+1, newPos[1]+1)[1]; p.erase(); p.remove()
+  elif self.pastMoves[-1][2] == (2,-2*(-1 if self.color-1 else 1)): p = board.PieceAt(newPos[0]-1, newPos[1]+1)[1]; p.erase(); p.remove()
+def onAttacked(self): self.remove(); return 'move'
+  
