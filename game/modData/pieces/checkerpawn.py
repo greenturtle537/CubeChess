c776c93e641402a2344a91a54c5bd58cd4359b0d
greenturtle537 <greenturtle537@mail.com>
2023-09-07 21:52:56 +0000

    Rebuilt client.py into a very simple TUI for testing

--- game/modData/pieces/checkerpawn.py
+++ game/modData/pieces/checkerpawn.py
@@ -1,18 +1,19 @@
 attackPath = []
 movePath = [(1,-1),(-1,-1)]
-canPromote = False
+canPromote = True
 canJumpPieces = True
 canJumpEmpties = True
-char = 'C'
-promotesTo = []
+char = 'c'
+promotesTo = ['C']
 
 conditionalMovePath = [
-  ((-2,-2), lambda p, b, g: b.PieceAt(p.pos[0]-1, p.pos[1]-1)[1])
+  ((-2,-2), lambda p, b, g: b.PieceAt(p.pos[0]-1, p.pos[1]-1)[1]),
   ((-2,-2), lambda p, b, g: b.PieceAt(p.pos[0]+1, p.pos[1]+1)[1])
 ]
 conditionalAttackPath = []
 def onMove(self, game, board, pieces, oldPos, newPos): 
   if self.pastMoves[-1][2] == (-2,-2*(-1 if self.color-1 else 1)): p = board.PieceAt(newPos[0]+1, newPos[1]+1)[1]; p.erase(); p.remove()
   elif self.pastMoves[-1][2] == (2,-2*(-1 if self.color-1 else 1)): p = board.PieceAt(newPos[0]-1, newPos[1]+1)[1]; p.erase(); p.remove()
+  if board.TileAt(newPos[0], newPos[1]) == (3 if self.color-1 else 2): self.promote() 
 def onAttacked(self): self.remove(); return 'move'
   
--- server/client.py
+++ server/client.py
@@ -8,27 +8,30 @@ from cursor import show_cursor
 from cursor import hide_cursor
 import time
 import sched
-
-print("You have connected to GlitchChat")
-username = input("Please enter your username: ")
-
-rows = 30
-command = ""
-
-scheduler = sched.scheduler(time.time, time.sleep)
-
-r = requests.get("http://glitchtech.top:8/connect",
-                 params={"username": username})
-result = r.json()
-print(result)
+import curses
+import curses.textpad
 
 
-def cls():
-  os.system('cls' if os.name == 'nt' else 'clear')
+#curses routines
+def start_curses(instance):
+  curses.noecho()
+  curses.cbreak()
+  stdscr.keypad(True)
 
 
-def en(input):
-  return base62.encodebytes(bytes(input, "utf-8"))
+def stop_curses(instance):
+  curses.nocbreak()
+  stdscr.keypad(False)
+  curses.echo()
+  curses.endwin()
+
+
+# requests routines
+def connect(username):
+  r = requests.get("http://glitchtech.top:8/connect",
+                   params={"username": username})
+  result = r.json()
+  return result
 
 
 def users():
@@ -43,40 +46,104 @@ def keepalive(userid):
   result = r.json()
 
 
-#Frameloop sequence ahead, todo error handling
-buffer = []
+# misc routines
+def cls():
+  os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def en(input):
+  return base62.encodebytes(bytes(input, "utf-8"))
+
+
+def center_text(text, y, pad="", attr=curses.A_NORMAL):
+  length = len(text)
+  stdscr.addstr(y, 0, pad * curses.COLS, attr)
+  num = round((curses.COLS / 2) - (length / 2))
+  stdscr.addstr(y, num, text, attr)
 
 
 def write(message):
   buffer.append(message)
-  cleaner()
+  refresh()
 
 
-def cleaner():
-  hide_cursor()
-  cls()
-  print("Command entry with keyboard & return, or ESC to exit")
-  for i in range(rows):
-    if i < len(buffer):
-      print(buffer[i])
-    else:
-      print("")
-  print("$:%s" % command, end="")
-  show_cursor()
-  if len(buffer) > rows:
+def refresh():
+  while len(buffer) > curses.LINES - 6:
     buffer.pop(0)
+  for i in buffer:
+    stdscr.addstr(buffer.index(i) + 3, 0, i)
+  stdscr.refresh()
 
 
 def newmsg(username):
   write(keepalive(username))
 
 
-def ping(username):
+def ping(method, args):
   if scheduler.empty():
-    scheduler.enter(0.5, 1, newmsg, (username, ))
+    scheduler.enter(0.5, 1, method, (args, ))
     scheduler.run()
 
 
+def counter(placeholder=""):
+  global count
+  count = count + 1
+  write(str(count))
+
+
+#Code entry
+
+#curses setup
+stdscr = curses.initscr()
+win = curses.newwin(1, curses.COLS - 10, curses.LINES - 2, 4)
+count = 2
+stdscr.nodelay(True)
+
+scheduler = sched.scheduler(time.time, time.sleep)
+start_curses(stdscr)
+buffer = []
+
+center_text("▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁", 0, "▓", curses.A_REVERSE)
+center_text("| GlitchChat v1.0 |", 1, "▓", curses.A_STANDOUT)
+center_text("▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔", 2, "▓", curses.A_REVERSE)
+
+center_text("", curses.LINES - 3, "▓")
+stdscr.addstr(curses.LINES - 2, 0, "[$]: ")
+
+tb = curses.textpad.Textbox(win, insert_mode=True)
+
+stdscr.refresh()
+command = ""
+start = time.time()
+
+while True:
+  c = stdscr.getch()
+  #6text = tb.edit()
+  #ping(counter, count)
+  if c == 27:  # Codes to escape(esc)
+    break  # Exit the while loop
+  elif c == curses.KEY_BACKSPACE or c == 127:  #Backspace is encoded as 127/DEL on chromebooks
+    command = command[0:len(command) - 1]
+    stdscr.addstr(curses.LINES - 2, 5, command + " ")
+    stdscr.addstr(curses.LINES - 2, 5,
+                  command)  #Wastefully corrects cursor position
+  elif c == curses.KEY_ENTER or c == 13 or c == 10:  # Accept carriage return and line feed
+    stdscr.addstr(curses.LINES - 2, 5, " " * len(command))
+    stdscr.addstr(curses.LINES - 2, 5, "")  #cursor correction
+    write(command)
+    command = ""
+  elif c > 31 and c <= 126:
+    command = command + chr(c)
+    stdscr.addstr(curses.LINES - 2, 5, command)
+  if time.time() - start > 1:
+    #write(str(count))
+    count = count + 1
+    start = time.time()
+  stdscr.refresh()
+stop_curses(stdscr)
+
+#Frameloop sequence ahead, todo error handling
+'''
 kb = KBHit()
 while True:
   if kb.kbhit():
@@ -87,3 +154,5 @@ while True:
       command = command[0:len(command) - 2:]
     else:
       command = command + c
+  
+'''
